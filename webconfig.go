fe9b7ea840405257e42be7ea1972ba72ec27e706
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2020-07-17 23:45:13 +0530

    Added properties file loading and minor error handling

--- webconfig.go
+++ webconfig.go
@@ -15,18 +15,44 @@ func helloWorld(writer http.ResponseWriter, request *http.Request) {
 
 //HandleRequests is an amazing func
 func HandleRequests() {
-	LoadProperties()
+	baseURL := GetFromProperties("application.properties", "breaking.bad.api.baseurl")
+	fmt.Println("Base url = ", baseURL)
+
 	http.HandleFunc("/hello", helloWorld)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 //LoadProperties loads properties
-func LoadProperties() {
-	props, _ := properties.LoadFile("application.properties", properties.UTF8)
-	val, ok := props.Get("hello")
-	if ok {
-		fmt.Printf("Property is %s", val)
-	} else {
-		fmt.Println("Error in reading props")
+func LoadProperties(propertiesFileName string) *properties.Properties {
+	props, error := properties.LoadFile(propertiesFileName, properties.UTF8)
+	if error == nil {
+		return props
+	}
+
+	log.Panicf("Property file with name %s not loaded with error : %s", propertiesFileName, error.Error())
+	return nil
+}
+
+//GetFromProperties is GetFromProperties
+func GetFromProperties(propertiesFileName string, propertyName string) string {
+	defer recoverPropertyGet()
+	properties := LoadProperties(propertiesFileName)
+	if properties != nil {
+		prop, ok := properties.Get(propertyName)
+
+		if ok {
+			return prop
+		}
+
+		log.Panicf("No property found with name %s", propertyName)
+		return ""
+	}
+	log.Panicf("Properties not loaded from filename : %s", propertiesFileName)
+	return ""
+}
+
+func recoverPropertyGet() {
+	if r := recover(); r != nil {
+		log.Println("Recovered from ", r)
 	}
 }
